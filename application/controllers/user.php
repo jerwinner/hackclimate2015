f151e21aeecd7bc1db8a1148a50518846a6db8be
Jerwin <jerwin.cruda@gmail.com>
2015-09-05 21:49:03 +0800

    view profile

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -95,4 +95,16 @@ class User extends MY_Controller{
         echo json_encode($data);
     }
 
+    function view_profile($uid){
+        $name = $this->model->get_value("name", "users", array("id" => $uid));
+
+        $data = array(
+            'name' => $name
+        );
+
+        $this->load->view('viewprofile', $data);
+    }
+
+
+
 }
